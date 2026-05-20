33f22cda54074ba29512c8b758669fba0610ebd6
Emanuel Benitez <emanuel.benitez@unc.edu.ar>
2026-05-19 23:58:04 -0300

    agrego en CORS el dominio del frontend en Vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -27,7 +27,9 @@ async function bootstrap() {
  
   // Habilitá CORS
   app.enableCors({
-    origin: 'http://127.0.0.1:5500',
+    origin: ['http://127.0.0.1:5500',
+      'https://control-gastos-iw-2.vercel.app'
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
